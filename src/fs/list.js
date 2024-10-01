56767c2a82e735be0c0d09d19d8377c43c33bcc6
Denis Saveliev <denissaveliev23@gmail.com>
2024-10-01 11:56:12 +0300

    feat: add solution to list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import { promises as fs } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 const list = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const filesDir = join(__dirname, 'files');
+
+  try {
+    await fs.access(filesDir);
+  } catch {
+    console.error('FS operation failed');
+    return;
+  }
+
+  try {
+    const files = await fs.readdir(filesDir);
+    console.log(files);
+  } catch (error) {
+    console.error('FS operation failed');
+  }
 };
 
 await list();
