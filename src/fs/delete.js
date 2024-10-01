98950fff99a4a952a6165942f8a5812f23ee5d9f
Denis Saveliev <denissaveliev23@gmail.com>
2024-10-01 11:52:27 +0300

    feat: add solution to remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const remove = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filesDir = path.join(__dirname, 'files');
+  const fileToRemovePath = path.join(filesDir, 'fileToRemove.txt');
+
+  try {
+    await fs.access(fileToRemovePath);
+  } catch {
+    console.error('FS operation failed');
+    return;
+  }
+
+  await fs.unlink(fileToRemovePath);
+  console.log('File removed');
 };
 
 await remove();
