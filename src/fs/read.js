70fcddeb71a8548fc65a239a6fec6431e3f20e92
Denis Saveliev <denissaveliev23@gmail.com>
2024-10-01 11:58:32 +0300

    feat: add solution to read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { promises as fs } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const fileToReadPath = join(__dirname, 'files', 'fileToRead.txt');
+
+  try {
+    const content = await fs.readFile(fileToReadPath, 'utf-8');
+    console.log(content);
+  } catch {
+    console.error('FS operation failed');
+  }
 };
 
 await read();
