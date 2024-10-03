f340d5b1209f4e4e58b45638fa3d2a758d141da7
Denis Saveliev <denissaveliev23@gmail.com>
2024-10-03 10:12:56 +0300

    feat: add solve to calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const stream = createReadStream(filePath);
+
+  try {
+    await pipeline(stream, hash);
+    console.log(hash.digest('hex'));
+  } catch (error) {
+    console.error('Error calculateHash:', error);
+  }
 };
 
 await calculateHash();
