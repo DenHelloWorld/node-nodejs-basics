2c68812f8f978659f2a18e227e2e6127be168d78
Denis Saveliev <denissaveliev23@gmail.com>
2024-10-03 11:01:45 +0300

    feat: add solve to parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const result = args
+    .map((arg, index) => {
+      if (arg.startsWith('--') && args[index + 1]) {
+        const key = arg.substring(2);
+        const value = args[index + 1];
+        return `${key} is ${value}`;
+      }
+      return null;
+    })
+    .filter(Boolean)
+    .join(', ');
+
+  process.stdout.write(result + '\n');
 };
 
 parseArgs();
