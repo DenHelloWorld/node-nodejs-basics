7bbb1987bdee2569490779ec4bde6dd21379a5ab
Denis Saveliev <denissaveliev23@gmail.com>
2024-10-05 17:12:51 +0300

    feat: add solve to zip/decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -6,22 +6,23 @@ import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const fileToCompress = path.join(__dirname, './files/fileToCompress.txt');
-const archive = path.join(__dirname, './files/archive.gz');
-const readStream = fs.createReadStream(fileToCompress);
-const writeStream = fs.createWriteStream(archive);
-const gzipStream = zlib.createGzip();
+const archiveFile = path.join(__dirname, './files/archive.gz');
 
 const compress = async () => {
+  const readStream = fs.createReadStream(fileToCompress);
+  const writeStream = fs.createWriteStream(archiveFile);
+  const gzip = zlib.createGzip();
+
   readStream
-    .pipe(gzipStream)
+    .pipe(gzip)
     .pipe(writeStream)
     .on('finish', () => {
       process.stdout.write(
-        `File ${fileToCompress} has been compressed to ${archive}`
+        `File ${fileToCompress} has been compressed to ${archive}\n`
       );
     })
     .on('error', (error) => {
-      process.stderr.write(`Error compress: ${error.message}`);
+      process.stderr.write(`Error compressing: ${error.message}\n`);
     });
 };
 
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToDeCompress = path.join(__dirname, './files/archive.gz');
+const fileToWrite = path.join(__dirname, './files/fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(fileToDeCompress);
+  const writeStream = fs.createWriteStream(fileToWrite);
+  const gunzip = zlib.createGunzip();
+
+  readStream
+    .pipe(gunzip)
+    .pipe(writeStream)
+    .on('finish', () => {
+      process.stdout.write(
+        `File ${fileToDeCompress} has been decompressed to ${fileToWrite}\n`
+      );
+    })
+    .on('error', (error) => {
+      process.stderr.write(`Error decompressing: ${error.message}\n`);
+    });
 };
 
 await decompress();
